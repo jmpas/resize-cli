654b5afa4ada8b9843c07fc4a8619317c10d6c4b
nipher <jonas@nipher.io>
2017-01-29 02:24:11 -0200

    Add suffix and better logs

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ import aspect from 'aspectratio'
 import imageSize from 'image-size'
 import meow from 'meow'
 import resizeImg from 'resize-img'
+import logSymbols from 'log-symbols'
 
-function writeFile(dest, fileName, buf) {
+function writeFile(dest, fileName, suffix = '', buf) {
   return new Promise((resolve, reject) => {
+    const arr = fileName.split('.')
+    const name = arr.slice(0, -1).join('.')
+    const extension = arr.pop()
+    const nameWithSuffix = `${name}${suffix}.${extension}`
     fs.writeFile(
-      path.resolve(dest, fileName),
+      path.resolve(dest, nameWithSuffix),
       buf,
-      err => err ? reject(err) : resolve(`${fileName} Saved!`))
+      err => err ? reject({msg: err, fileName: nameWithSuffix}) : resolve(nameWithSuffix))
   })
 }
 
@@ -43,38 +48,41 @@ const cli = meow(`
   Examples
     $ resize ./images/**/* -w 450 -h 450
     $ resize ./images/**/* -w 450 -h 450 -o ./dist/images/
-    $ resize ./images/**/* -w 450 -h 450 -o ./dist/images/**/* -s ".min"
+    $ resize ./images/**/* -w 450 -h 450 -o ./dist/images/ -s ".min"
 `, {
   alias: {
     w: 'max-width',
     h: 'max-height',
-    o: 'output'
+    o: 'output',
+    s: 'suffix'
   }
 })
 
 const {input: images, flags} = cli
-console.log(flags)
 
 const promises = images
   .map(imgPath => {
-    return {
-      imgPath,
-      fileName: imgPath.split('/').pop(),
-      size: sizeOf(imgPath)
-    }
+    const fileName = imgPath.split('/').pop()
+    const size = sizeOf(imgPath)
+
+    return {imgPath, fileName, size}
   })
   .map(async img => {
     const {width, height} = await img.size
     const {w, h} = flags
     const [newWidth, newHeight] = aspect.resize(width, height, w, h)
 
+    console.log(logSymbols.success, `Getting data from ${img.fileName}`)
+
     return Object.assign({}, img, {width: newWidth, height: newHeight})
   })
   .map(async img => {
     try {
       const {fileName, imgPath, width, height} = await img
+
       const imageBuffer = await getBuf(imgPath)
       const resizedBuffer = await resizeImg(imageBuffer, {width, height})
+      console.log(logSymbols.success, `Resizing ${fileName}`)
 
       return {
         fileName,
@@ -84,22 +92,23 @@ const promises = images
       return Promise.reject(err)
     }
   })
-    .map(async resizedImg => {
-      try {
-        const {fileName, buf} = await resizedImg
-        return writeFile(flags.o, fileName, buf)
-      } catch (err) {
-        return Promise.reject(err)
-      }
-    })
-    .map(async data => {
-      try {
-        console.log(await data)
-      } catch (err) {
-        console.log(err)
-      }
-    })
+  .map(async resizedImg => {
+    try {
+      const {fileName, buf} = await resizedImg
+      return writeFile(flags.o, fileName, flags.s, buf)
+    } catch (err) {
+      return Promise.reject(err)
+    }
+  })
+  .map(async data => {
+    try {
+      const fileName = await data
+      console.log(logSymbols.success, `Saving ${fileName}`)
+    } catch (err) {
+      return console.log(logSymbols.error, `Saving ${fileName}`)
+    }
+  })
 
 Promise.all(promises)
-  .then(() => console.log('All images were successfuly resized.'))
-  .catch(() => console.warn(`Couldn't resize all the images.`))
+  .then(() => console.log(logSymbols.success, 'All images were successfuly resized.'))
+  .catch((err) => console.log(logSymbols.warning, err))
